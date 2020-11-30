d176fb6174f75de4ddacb47613b2e1a88938f958
DEV-SSG\golfmon <rusifer@naver.com>
2020-11-30 16:05:30 +0900

    부동산 클래스 테스트

--- starcraft.py
+++ starcraft.py
@@ -26,15 +26,27 @@
 # attack(name, "1시", damage)
 # attack(tank_name, "1시", tank_damage)
 # attack(tank2_name, "1시", tank2_damage)
+from random import *
+
+
 class Unit:
     def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
         self.speed = speed
+        print("{0} 유닛이 생성되었습니다.".format(name))
 
     def move(self, location):
-        print("[지상유닛이름]")
+        # print("[지상유닛이름]")
         print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(self.name, location, self.speed))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+        else:
+            print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
 
 
 class AttackUnit(Unit):
@@ -45,13 +57,41 @@ class AttackUnit(Unit):
     def attack(self, location):
         print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(self.name, location, self.damage))
 
-    def damaged(self, damage):
-        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
-        self.hp -= damage
-        if self.hp <= 0:
-            print("{0} : 파괴되었습니다.".format(self.name))
+
+# 마린
+class Marine(AttackUnit):
+    def __init__(self):
+        AttackUnit.__init__(self, "마린", 40, 1, 5)
+
+    # 스팀팩 : 일정시간 동안 이동 및 공격속도 증가, 체력 10 감소
+    def stimpack(self):
+        if self.hp > 10:
+            self.hp -= 10
+            print("{0} : 스팀팩을 사용합니다. (HP 10감소)".format(self.name))
         else:
-            print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+            print("{0} : 체력이 부족하여 스탬팩을 사용할 수 없습니다.".format(self.name))
+
+
+# 탱크
+class Tank(AttackUnit):
+    seize_developed = False
+
+    def __init__(self):
+        AttackUnit.__init__(self, "시즈탱크", 150, 1, 35)
+        self.seize_mode = False;
+
+    def set_seize_mode(self):
+        if not Tank.seize_developed:
+            return
+
+        if not self.seize_mode:
+            print("{0} : 시즈모드로 전환합니다.".format(self.name))
+            self.damage *= 2
+            self.seize_mode = True
+        else:
+            print("{0} : 시즈모드로 해제합니다.".format(self.name))
+            self.damage /= 2
+            self.seize_mode = False
 
 
 class Flyable:
@@ -68,18 +108,22 @@ class FlyableAttackUnit(AttackUnit, Flyable):
         Flyable.__init__(self, flying_speed)
 
     def move(self, location):
-        print("[공중유닛이동]")
+        # print("[공중유닛이동]")
         self.fly(self.name, location)
 
 
-class BuildingUnit(Unit):
-    def __init__(self, name, hp, location):
-        # Unit.__init__(self, name, hp, 0)
-        super().__init__(name, hp, 0)
-        self.location = location
+class Wraith(FlyableAttackUnit):
+    def __init__(self):
+        FlyableAttackUnit.__init__(self, "레이쓰", 80, 20, 5)
+        self.clocked = False
 
-
-supply_depot = BuildingUnit("서플라이디폿", 500, "7시")
+    def clocking(self):
+        if self.clocked:
+            print("{0} : 클로킹모드를 설정합니다.".format(self.name))
+            self.clocked = False
+        else:
+            print("{0} : 클로킹모드를 해제합니다.".format(self.name))
+            self.clocked = True
 
 
 def game_start():
@@ -87,14 +131,41 @@ def game_start():
 
 
 def game_over():
-    pass
+    print("Player: gg")
+    print("[Player] 님이 게임에서 퇴장하셨습니다.")
 
 
 game_start()
-vulture = AttackUnit("벌쳐", 80, 10, 20)
-battelcruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3);
 
-vulture.move("11시")
-battelcruiser.move("9시")
+m1 = Marine()
+m2 = Marine()
+m3 = Marine()
+
+t1 = Tank()
+t2 = Tank()
+
+w1 = Wraith()
+
+attack_units = [m1, m2, m3, t1, t2, w1]
+
+for unit in attack_units:
+    unit.move("1시")
+
+Tank.seize_developed = True
+print("[알림] 탱크 시즈 모드 개발이 완료되었습니다.")
+
+for unit in attack_units:
+    if isinstance(unit, Marine):
+        unit.stimpack()
+    elif isinstance(unit, Tank):
+        unit.set_seize_mode()
+    elif isinstance(unit, Wraith):
+        unit.clocking()
+
+for unit in attack_units:
+    unit.attack("1시")
+
+for unit in attack_units:
+    unit.damaged(randint(5, 21))
+
 game_over()
-
