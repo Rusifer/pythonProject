4d836ae84de6e18622da523d6a1c24668a088578
DEV-SSG\golfmon <rusifer@naver.com>
2020-12-03 15:38:42 +0900

    사용자정의에러함수

--- exception2.py
+++ exception2.py
@@ -1,5 +1,9 @@
 class BigNumberError(Exception):
-    pass
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
 
 
 try:
@@ -7,9 +11,10 @@ try:
     num1 = int(input("첫 번째 숫자를 입력하세요: "))
     num2 = int(input("두 번째 숫자를 입력하세요: "))
     if num1 >= 10 or num2 >= 10:
-        raise BigNumberError
+        raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
 except ValueError:
     print("잘못된 값을 입력하였습니다. 한 자리 정수만 입력하세요")
-except BigNumberError:
+except BigNumberError as err:
     print("에러가 발생하였습니다. 한자리 숫자만 입력하세요")
+    print(err)
